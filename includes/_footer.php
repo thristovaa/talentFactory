6fd1c7ff49a918399aa467471c4c975f6beff242
Raya Hristova <raya.thristova@gmail.com>
2022-09-04 22:02:16 +0300

    footer

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,4 +1,4 @@
-<footer id="main-footer" class="row justify-content-center">
+<!--<footer id="main-footer" class="row justify-content-center">
     <div class="footer col-lg-6 col-md-12 justify-content-center d-flex flex-column m-auto">
         <h4 class="footer-up m-auto">Talent Factory</h4>
         <div class="text-center d-flex flex-column m-auto">
@@ -17,4 +17,74 @@
             <a href="mailto:cool@panda.com">talent@factory.com</a>
 		</div>
     </div>
+</footer>-->
+
+<footer class="text-center text-white" style="background-color: #f0d3f7;">
+  <!-- Grid container -->
+  <div class="container pt-4">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+      <!-- Facebook -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-facebook-f"></i
+      ></a>
+
+      <!-- Twitter -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-twitter"></i
+      ></a>
+
+      <!-- Google -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-google"></i
+      ></a>
+
+      <!-- Instagram -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-instagram"></i
+      ></a>
+
+      <!-- Linkedin -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-linkedin"></i
+      ></a>
+      <!-- Github -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-github"></i
+      ></a>
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2022 Copyright:
+    <a class="text-dark">TalentFactory®</a>
+  </div>
+  <!-- Copyright -->
 </footer>
