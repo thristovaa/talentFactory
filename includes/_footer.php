e87dfeb750df2d9f831e8b70542853bdb494fbc3
Vasilena Stanoyska <v.stanoyska@gmail.com>
2022-09-04 17:26:51 +0300

    home

---
+++ includes/_footer.php
@@ -0,0 +1,20 @@
+<footer id="main-footer" class="row justify-content-center">
+    <div class="footer col-lg-6 col-md-12 justify-content-center d-flex flex-column m-auto">
+        <h4 class="footer-up m-auto">Talent Factory</h4>
+        <div class="text-center d-flex flex-column m-auto">
+			<div class="justify-content-center d-flex m-auto">
+        	    <a class="nav-link" href="https://www.facebook.com" target="_blank">
+					<i class="fas fa-shopping-bag"></i>
+				</a>
+        	    <a class="nav-link" href="https://www.instagram.com" target="_blank">
+					<i class="fab fa-instagram"></i>
+				</a>
+        	    <a class="nav-link" href="https://www.facebook.com/bqlatastaq" target="_blank">
+					<i class="fab fa-facebook"></i>
+				</a>
+        	</div>
+            <a href="tel:+3598 8538506">+359 88538 0506</a>
+            <a href="mailto:cool@panda.com">talent@factory.com</a>
+		</div>
+    </div>
+</footer>
---
+++ includes/_navbar.php
@@ -0,0 +1,11 @@
+<div class="_navbar">
+	<nav class="navbar navbar-expand-lg navbar-light">
+		<a class="navbar-brand" href="/talentFactory">Talent Factory</a>
+	  	  <div class="navbar-nav d-flex flex-row">
+				<a class="nav-item nav-link p-2" href="./explore.php">Explore</a>
+	  	    	<a class="nav-item nav-link p-2" href="./create.php">Create</a>
+				<a class="nav-item nav-link p-2" href="./profile.php">
+					<i class="fa-sharp fa-solid fa-circle-user fa-lg"></i></a>
+	  	  </div>
+	</nav>
+</div>
