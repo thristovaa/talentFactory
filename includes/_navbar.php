05b5124ff01fa74d3253abc6234f6b3cf58fa666
Vasilena Stanoyska <v.stanoyska@gmail.com>
2022-09-04 23:05:02 +0300

    promeni

--- includes/_navbar.php
+++ includes/_navbar.php
@@ -14,14 +14,16 @@
 </div>-->
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
-  	<div class="navbar-header d-flex flex-row">
+  	<div class="navbar-header">
       <a class="navbar-brand p-2" href="./index.php">Talent Factory</a>
-		<ul class="nav navbar-nav d-flex flex-row" style="font-size: 20px;">
+    </div>
+	<div class="nav navbar-nav d-flex flex-row">
+		<ul class="nav navbar-nav d-flex flex-row align-items-center" style="font-size: 20px;">
 			<li class="p-2"><a href="./index.php" class="text-decoration-none">Home</a></li>
 			<li class="p-2"><a href="./explore.php" class="text-decoration-none">Explore</a></li>
 			<li class="p-2"><a href="./create.php" class="text-decoration-none">Create</a></li>
 		</ul>
-    </div>
-    <button class="btn navbar-btn" style="background-color: #0ad6b4;">Button</button>
+    	<button class="btn navbar-btn m-2" style="background-color: #0ad6b4;">Button</button>
+	<div>
   </div>
 </nav>
