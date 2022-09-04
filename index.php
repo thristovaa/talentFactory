fb6e4b3fa489b60fe0d7f4f5c2121cd7f573f9fb
Raya Hristova <raya.thristova@gmail.com>
2022-09-04 21:45:03 +0300

    html and css nav

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include "./includes/db.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -5,49 +6,25 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./css/style.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/bootstrap.min.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap%27" rel="stylesheet">
-		<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+		    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
         <title>Talent Factory</title>
     </head>
     <body>
 		<?php include "./includes/_navbar.php";?>
-        <!--<div class="container">
-            <form action="./includes/login.php" class="text-start mb-3" method="post">
-              <div class="form-floating mb-4">
-                <input type="text" class="form-control" placeholder="Email" name="username" id="loginEmail">
-                <label for="loginUsername">Потребителско име</label>
-              </div>
-              <div class="form-floating mb-4">
-                <input type="password" class="form-control" placeholder="Password" name="password" id="loginPassword">
-                <label for="loginPassword">Парола</label>
-              </div>
-              <button class="btn btn-soft-peach rounded-pill me-1 btn-login w-100 mb-2" name="login" type="submit">Log In</button>
-            </form>
-        </div>-->
-        <h1 style="font-family: 'Lobster', cursive;">Заглавие</h1>
-        <p style="font-family: 'Raleway', sans-serif;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="container mt-3">
+            <h1 style="font-family: 'Lobster', cursive;">Заглавие</h1>
+        </div>
         <div class="container">
-            <div class="row">
-              <div class="col-sm">
-                One of three columns
-              </div>
-              <div class="col-sm">
-                One of three columns
-              </div>
-              <div class="col-sm">
-                One of three columns
-              </div>
-            </div>
+            <p style="font-family: 'Raleway', sans-serif;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
 		<?php include "./includes/_footer.php";?>
     </body>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-	<script type="module" 
-		src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-	<script nomodule 
-		src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-	<script src="https://kit.fontawesome.com/3077a13519.js" crossorigin="anonymous"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="https://kit.fontawesome.com/3077a13519.js" crossorigin="anonymous"></script>
+    <script src="./js/bootstrap.bundle.min.js"></script>
 </html>
