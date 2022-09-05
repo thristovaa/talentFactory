ebbdf416a369d37c938171363856382e6398ebb9
Raya Hristova <raya.thristova@gmail.com>
2022-09-05 14:16:04 +0300

    nav

--- includes/_navbar.php
+++ includes/_navbar.php
@@ -1,29 +1,19 @@
-<!--<div class="_navbar">
-	<nav class="navbar navbar-expand-lg navbar-light">
-		<a class="navbar-brand" href="/talentFactory">Talent Factory</a>
-	  	  <div class="navbar-nav d-flex flex-row">
-				<a class="nav-item nav-link p-2" href="./explore.php">Explore</a>
-	  	    	<a class="nav-item nav-link p-2" href="./create.php">Create</a>
-				<?php if(isset($_SESSION['username'])) { ?>
-					<i class="fa-sharp fa-solid fa-circle-user fa-lg"></i></a>
-				<?php } else { ?>
-					<button type="button" id="myModal" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Login</button>
-				<?php } ?>
-	  	  </div>
-	</nav>
-</div>-->
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
   	<div class="navbar-header">
-      <a class="navbar-brand p-2" href="#">WebSiteName</a>
+      <a class="navbar-brand p-2" href="#">Talent Factory</a>
     </div>
 	<div class="nav navbar-nav d-flex flex-row">
 		<ul class="nav navbar-nav d-flex flex-row align-items-center" style="font-size: 20px;">
 			<li class="m-2"><a href="#" class="text-decoration-none">Home</a></li>
-			<li class="m-2"><a href="#" class="text-decoration-none">Link</a></li>
-			<li class="m-2"><a href="#" class="text-decoration-none">Link</a></li>
+			<li class="m-2"><a href="#" class="text-decoration-none">Explore</a></li>
+			<li class="m-2"><a href="#" class="text-decoration-none">Create</a></li>
+			<?php if(isset($_SESSION['username'])) { ?>
+				<li class="m-2"><i class="fa-sharp fa-solid fa-circle-user fa-lg"></i></a></li>
+			<?php } else { ?>
+			<button type="button" id="myModal" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Login</button>
+			<?php } ?>
 		</ul>
-    	<button class="btn navbar-btn m-2" style="background-color: #0ad6b4;">Button</button>
 	</div>
   </div>
 </nav>
