9ce5aa84d2bc4a70f4a8c1e13de2ff6b53f93c8e
Vasilena Stanoyska <v.stanoyska@gmail.com>
2022-09-06 20:47:33 +0300

    _navbar

--- includes/_navbar.php
+++ includes/_navbar.php
@@ -11,6 +11,12 @@
 		<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
         <link rel="icon" type="image/png" href="./images/logo/star_pink.png">
         <title>Talent Factory</title>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js">		</script>
+    	<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    	<script src="https://kit.fontawesome.com/3077a13519.js" crossorigin="anonymous"></script>
+    	<script src="./js/bootstrap.bundle.min.js"></script>
     </head>
 	<body>
 		<nav class="navbar navbar-inverse" style="background-color: #010028">
