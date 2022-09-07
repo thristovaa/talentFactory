7d7871cc8a63cbe1becbc8d49e84caf201fa729c
Vasilena Stanoyska <v.stanoyska@gmail.com>
2022-09-07 15:48:53 +0300

    final promeni

--- explore.php
+++ explore.php
@@ -15,9 +15,6 @@
 				<li class="nav-item" role="presentation">
 					<button type="button" id="tabList-4" class="nav-link exp-link" data-bs-toggle="tab" data-bs-target="#tabList-pane-4" role="tab" aria-controls="tabList-pane-4" aria-selected="false">Photography</button>
 				</li>
-				<li class="nav-item" role="presentation">
-					<button type="button" id="tabList-5" class="nav-link exp-link" data-bs-toggle="tab" data-bs-target="#tabList-pane-5" role="tab" aria-controls="tabList-pane-5" aria-selected="false">Video</button>
-				</li>
 			</ul>
 		</div>
 		<div class="tab-content">
@@ -158,47 +155,6 @@
 						$create_user_image = $row['create_author_image'];
 						
 						if($create_category == 'Photography'){
-					?>
-					<div class="col-lg-4 col-md-12 col-sm-12 my-4 exp-card">
-						
-						<a href="./post.php?p_id=<?php echo $create_id; ?>">
-							<div class="exp-img m-auto rounded" style="background-image: url('./images/create/<?php echo $create_image; ?>'); background-size: cover;
-							background-position: center center;">
-								<div class="exp-info w-50 h-20">
-									<img class="card-title pb-2" width="50px" style="border-radius:50%;" src="./images/pfp/<?php echo $create_user_image?>">
-									<h5 class="card-title">By <?php echo $create_author; ?></h5>
-									<p class="card-text">★ <?php echo $create_rating ?>/5</p>
-								</div>
-							</div>
-						</a>
-					</div>
-					<?php }
-					else{continue;}
-				} ?>
-				</div>
-	
-				<!--dotuk-->
-			</div>
-			<div id="tabList-pane-5" class="tab-pane" role="tabpanel" aria-labelledby="tabList-5">
-				<!--ottuk-->
-	
-				<div class="row row-cols-1 row-cols-md-3 m-auto">
-					<?php 
-	      
-					$query = "SELECT * FROM posts ";
-					$query .= "ORDER BY create_id DESC";
-	
-					$select_video_posts_query = mysqli_query($connect, $query);
-					while($row = mysqli_fetch_assoc($select_video_posts_query)){
-						$create_id = $row['create_id'];
-						$create_title = $row['create_title'];
-						$create_rating = $row['create_rating'];
-						$create_image = $row['create_image'];
-						$create_author = $row['create_author'];
-						$create_category = $row['create_category'];
-						$create_user_image = $row['create_author_image'];
-						
-						if($create_category == 'Videos'){
 					?>
 					<div class="col-lg-4 col-md-12 col-sm-12 my-4 exp-card">
 						
