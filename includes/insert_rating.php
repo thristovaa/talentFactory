082a42a32e3eed3543b535bfcbc9c217d225c051
Raya Hristova <raya.thristova@gmail.com>
2022-09-07 14:37:32 +0300

    rating system

--- includes/insert_rating.php
+++ includes/insert_rating.php
@@ -15,11 +15,12 @@ if(isset($_POST['submit_rating']))
 
   echo $php_rating;
 
-  $query = "INSERT INTO test(php) ";
-  $query .= "VALUES(
-    '" . mysqli_real_escape_string($connect, $php_rating) . "'
-    )";
-  $insert=mysqli_query($connect, $query);
+  $query = "UPDATE posts SET ";
+  $query .= "create_rating = 
+            '" . mysqli_real_escape_string($connect, $php_rating) . "' ";
+  $query .= "WHERE create_id = {$p_id}" ;
+
+  $insert = mysqli_query($connect, $query);
   if(!$insert){
     die("QUERY FAILED " . mysqli_error($connect) . ' ' . mysqli_errno($connect));
 }
